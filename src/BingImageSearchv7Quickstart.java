80a41145745b32fc5a14a70ab27fd4c698f8a987
Josef <josef-karlsson@outlook.com>
2020-08-07 17:06:57 +0200

    first commit

---
+++ src/BingImageSearchv7Quickstart.java
@@ -0,0 +1,138 @@
+/*Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the MIT License.*/
+
+import com.google.gson.*;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Gson: https://github.com/google/gson
+ * Maven info:
+ *     groupId: com.google.code.gson
+ *     artifactId: gson
+ *     version: 2.8.1
+ *
+ * Once you have compiled or downloaded gson-2.8.1.jar, assuming you have placed it in the
+ * same folder as this file (BingImageSearch.java), you can compile and run this program with the following steps:
+ * 1. create three directories, named "bin", "src", and "lib"
+ * 2. place this .java file in "src"
+ * 3. download the latest gson .jar file online, and place it in the "lib" folder
+ * 4. replace the subscriptionKey value with your valid Cognitive services subscription key
+ * 5. run the following commands, replacing "lib/gson-2.8.5.jar" with your .jar file:
+ * javac -d bin -sourcepath src -cp lib/gson-2.8.5.jar src/BingImageSearchv7Quickstart.java
+ * java -cp bin;lib/gson-2.8.5.jar BingImageSearchv7Quickstart
+ */
+
+class BingImageSearchv7Quickstart {
+
+// ***********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+    // Bing Search V7 subscription key.
+    static String subscriptionKey = "675f17090916467cbdb6e24daa409f8e";
+
+    // Bing Search V7 endpoint.
+    static String host = "https://moviecollector.cognitiveservices.azure.com/";
+
+    static String path = "/bing/v7.0/images/search";
+
+    static String searchTerm = "1917 cover";
+
+
+    public static SearchResults SearchImages (String searchQuery) throws Exception {
+
+        // construct URL of search request (endpoint + query string)
+        URL url = new URL(host + path + "?q=" +  URLEncoder.encode(searchQuery, "UTF-8"));
+        HttpsURLConnection connection = (HttpsURLConnection)url.openConnection();
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+        // receive JSON body
+        InputStream stream = connection.getInputStream();
+        String response = new Scanner(stream).useDelimiter("\\A").next();
+
+        // construct result object for return
+        SearchResults results = new SearchResults(new HashMap<String, String>(), response);
+
+        // extract Bing-related HTTP headers
+        Map<String, List<String>> headers = connection.getHeaderFields();
+        for (String header : headers.keySet()) {
+            if (header == null) continue;      // may have null key
+            if (header.startsWith("BingAPIs-") || header.startsWith("X-MSEdge-")) {
+                results.relevantHeaders.put(header, headers.get(header).get(0));
+            }
+        }
+
+        stream.close();
+        return results;
+    }
+
+    // pretty-printer for JSON; uses GSON parser to parse and re-serialize
+    public static String prettify(String json_text) {
+        JsonParser parser = new JsonParser();
+        JsonObject json = parser.parse(json_text).getAsJsonObject();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return gson.toJson(json);
+    }
+
+    public static void main (String[] args) {
+       /* if (subscriptionKey.length() != 32) {
+            System.out.println("Invalid Bing Search API subscription key!");
+            System.out.println("Please paste yours into the source code.");
+            System.exit(1);
+        }*/
+
+        try {
+            String fileName = "E://Movies/movies.txt";
+            Path moviePath = Paths.get(fileName);
+            Scanner scanner = new Scanner(moviePath);
+
+            try(FileWriter fw = new FileWriter("E:\\Movies/movieCoversURLs.txt", true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw))
+            {
+                while (scanner.hasNextLine()) {
+                    String searchTerm = scanner.nextLine().split("\\.")[0];
+                    System.out.println("Searching the Web for: " + searchTerm + " poster");
+                    SearchResults result = SearchImages(searchTerm + " poster");
+                    JsonParser parser = new JsonParser();
+                    JsonObject json = parser.parse(result.jsonResponse).getAsJsonObject();
+                    //String total = json.get("totalEstimatedMatches").getAsString();
+                    JsonArray results = json.getAsJsonArray("value");
+                    JsonObject first_result = (JsonObject)results.get(0);
+                    String resultURL = first_result.get("thumbnailUrl").getAsString();
+                    out.println(resultURL);
+                    TimeUnit.MILLISECONDS.sleep(500);
+
+                } scanner.close();
+
+            } catch (IOException e) {
+                //exception handling left as an exercise for the reader
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace(System.out);
+            System.exit(1);
+        }
+    }
+}
+
+// Container class for search results encapsulates relevant headers and JSON data
+class SearchResults{
+    HashMap<String, String> relevantHeaders;
+    String jsonResponse;
+    SearchResults(HashMap<String, String> headers, String json) {
+        relevantHeaders = headers;
+        jsonResponse = json;
+    }
+}
---
+++ src/Frame.java
@@ -0,0 +1,86 @@
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Frame {
+    private static final int MOVIE_ICONE_WIDTH = 250;
+    private static final int MOVIE_ICONE_HEIGHT = 400;
+
+    public static void main(String[] args) throws IOException {
+        JFrame frame = new JFrame("Movie collecter");
+        frame.setSize(1500, 3000);
+
+
+        JPanel panel = new JPanel(new GridLayout(0, 8, 25, 25));
+        panel.setBackground(Color.DARK_GRAY);
+
+        String fileName = "E://Movies/movies.txt";
+        Path path = Paths.get(fileName);
+        Scanner scanner = new Scanner(path);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (!(line.equals(""))) {
+
+                ImageIcon icon = new ImageIcon("C:\\Users\\Josef\\PycharmProjects\\Movie-Collector\\images\\" + line.split("\\.")[0] + ".JPG");
+
+                // Rescale the image to fit the button
+                Image img = icon.getImage() ;
+                Image newimg = img.getScaledInstance(MOVIE_ICONE_WIDTH, MOVIE_ICONE_HEIGHT,  java.awt.Image.SCALE_SMOOTH ) ;
+                icon = new ImageIcon( newimg );
+
+                JButton button = new JButton(line, icon);
+
+                // Makes the button bordesrs "invisable"
+                button.setBackground(Color.DARK_GRAY);
+                button.setForeground(Color.DARK_GRAY);
+                button.setBorder(new LineBorder(Color.DARK_GRAY));
+
+                button.setPreferredSize(new Dimension(MOVIE_ICONE_WIDTH, MOVIE_ICONE_HEIGHT));
+
+                button.addActionListener(new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        File file = new File("E:/Movies/Movies/" + line.split("\\.")[0] + "/" + line);
+                        Desktop desktop = Desktop.getDesktop();
+
+                        if(!(file.exists())) {
+                            file = new File("F:/Film/" + line.split("\\.")[0] + "/" + line);
+                        }
+
+                        if(file.exists()) {
+                            try {
+                                desktop.open(file);
+                            } catch (IOException ioException) {
+                                ioException.printStackTrace();
+                            }
+                        }
+
+                    }
+                });
+                panel.add(button);
+            }
+        }
+        scanner.close();
+
+        // Makes the frame scrolleble
+        JScrollPane scrPane = new JScrollPane(panel);
+        scrPane.getVerticalScrollBar().setUnitIncrement(20);
+
+
+        // And JPanel needs to be added to the JFrame itself!
+        frame.add(scrPane);
+        frame.setVisible(true);
+
+    }
+
+}
+
